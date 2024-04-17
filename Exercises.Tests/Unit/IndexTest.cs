babac1e10e1710edcb62373664fb78488b62a5e1
ArjunKrishnaD <arjkr@eg.dk>
2024-04-17 19:06:01 +0530

    Added Index logs

---
+++ Exercises.Tests/Unit/IndexTest.cs
@@ -0,0 +1,19 @@
+﻿
+using Engine.Procedures;
+using Xunit;
+
+namespace Engine.Tests.Unit
+{
+    public class IndexTest
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.Equal("1", new IndexId().Next().Value());
+            Assert.Equal("2", new IndexId().Next().Next().Value());
+            Assert.Equal("1.1", new IndexId().Next().Down().Value());
+            Assert.Equal("1.2", new IndexId().Next().Down().Next().Value());
+            Assert.Equal("1", new IndexId().Next().Down().Next().Up().Value());
+        }
+    }
+}
---
+++ Exercises/Procedures/IndexId.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Procedures
+{
+    public class IndexId
+    {
+        private readonly List<int> ids = new List<int> {0};
+        public IndexId Next()
+        {
+            ids[ids.Count - 1]= ids[ids.Count - 1] + 1;
+            return this;
+        }
+        public IndexId Down()
+        {
+            ids.Add(1);
+            return this;
+        }
+        public IndexId Up()
+        {
+            ids.RemoveAt(ids.Count-1);
+            return this;
+        }
+        public string Value()
+        {
+            return ids.Select(i => i.ToString()).Aggregate((i, j) => i + "." + j);
+
+        }
+
+    }
+}
